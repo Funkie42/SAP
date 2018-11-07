6eaee9c95da7dca2c6235cdb30bf37b9c2afbc67
Funkie42 <marius.funk@gmx.de>
2018-11-07 16:16:57 +0100

    datei auslesen hinzugefügt

---
+++ SAT1/src/naiveSolution/Gameboard.java
@@ -0,0 +1,16 @@
+package naiveSolution;
+
+import javafx.animation.*;
+
+
+
+public class Gameboard {
+
+	public Gameboard{
+		
+	}
+	
+	public static void main(String[] args) { // existing main method
+		new Gameboard(); // add this line
+	}
+}
--- SAT1/src/naiveSolution/LeseDateiEin.java
+++ SAT1/src/naiveSolution/LeseDateiEin.java
@@ -1,21 +1,68 @@
 package naiveSolution;
 
 import java.io.*;
+import java.util.Scanner;
+
 
 
 public class LeseDateiEin {
 
-	public static void main(String[] args) {
-		//File inputData = new File("../../ueb1/u01puzzle-small1.txt");
-		File inputData = new File("/home/marius/Stuff/SAP/SAT1/ueb1/u01puzzle-small2.txt");
+	public static void main(String[] args) throws IOException { 
+		// read data from file
+		File inputData = new File("ueb1/u01puzzle-small1.txt");
+		
 		BufferedReader buffInput = new BufferedReader(new FileReader(inputData));
 		
-		String st;
+		int st;
 		
-		while ((st = buffInput.readLine()) != null) {
-			System.out.println(st);
+		// init Gamegrid
+		int[][] data = {{},{}}; 
+		int gameLines = 0;
+		int gameRows = 0;
+		
+		// Read inputdata
+		int i = 0;
+		int j = 0;
+		while ((st = buffInput.read()) != -1) {
+			
+			st = st;
+			
+			//Read lenght and width of game
+			if(i==0 && j==0) gameLines = st;
+			if(i==0 && j==2){
+				gameRows = st;
+				data = new int[gameLines][gameRows];
+				
+			}
+			
+			// "?" = 0 in the array
+			if((char)st == '?') {
+				data[i-1][j] = 0;
+			}
+			// "W" = 1 in the array
+			if((char)st == 'W') {
+				data[i-1][j] = 1;
+			}
+			// "B" = 2 in the array
+			if((char)st == 'B') {
+				data[i-1][j] = 2;
+			}
+			
+			j++;
+			
+			// On linebreak, start at next row
+			if((char)st == '\n') {
+				i++;
+				j = 0;
+			}
 		}
-	
+		System.out.println(data[1].length);
+		for(int a=0; a < data.length/8; a++) {
+			for(int b=0; b < data[b].length/8; b++) {
+				System.out.print(data[a][b]);
+			}
+			System.out.println("\n");
+		}
 	}
 	
 }
