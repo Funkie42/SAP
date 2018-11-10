578fa7d8b4eceb4ba0c15b6f99b34166c3938ba9
1MarcoP <marco.philip@online.de>
2018-11-10 19:51:07 +0100

    updated pico reader

--- SAT1/readFile.py
+++ SAT1/readFile.py
@@ -1,4 +1,5 @@
 import subprocess
+import interface
 def readFileData(fileName):
     fo = open(fileName)
 
@@ -37,40 +38,25 @@ def readPicosatSolution(result):
     print("Given result:",result)
     result = result.stdout.decode('utf-8').splitlines()
     print("The given formula is: ", result[0].replace("s ",""))
-    # only take the clauses (written in the second line)
-    result = result[1].replace("v ", "").split(" ")
-    # result contains all values as string
-    lastItem = result[len(result)-2]
-    lines = int(lastItem[:2])
-    #cols = lastIteint(result[sackCounter])m[2]+lastItem[3]
-    cols = 2
-    if(lines < 0):
-        lines = lines * -1
-    if(cols < 0):
-        cols = cols * -1
-    print(lines)
+    # remove the frist line
+    result.pop(0)
+    values = []
+    for i in result:
+        z = i.split()
+        for j in z:
+            if(j!="v"):
+                values.append(j)
     sack = []
-    sackCounter = 0
-    for i in range(lines):
-        miniSack = []
-        for j in range(cols):
-            state = 0
-            if(sackCounter > len(result)-1):
-                return sack
-            if(int(result[sackCounter]) == 0):
-                sackCounter = sackCounter +1
-                break
-            if(int(result[sackCounter]) > 0):
-               state = 1  # for white block
-            if(int(result[sackCounter]) < 0):
-                state = 2 # for black block
-            miniSack.append(state)
-            sackCounter = sackCounter + 1
-        sack.append(miniSack)
-    return sack
+    counter = 0
+    for i in range(interface.lines):
+        minisack = []
+        for j in range(interface.cols):
+            minisack.append(values[counter])
+            counter = counter +1
+        sack.append(minisack)
+    print(sack)
 
 if __name__ == "__main__":
     # conde for calling picosat
-    result = subprocess.run(['picosat', '../picosat-965/simple.cnf'], stdout=subprocess.PIPE)
+    result = subprocess.run(['picosat', 'satTest.cnf'], stdout=subprocess.PIPE)
     x = readPicosatSolution(result)
-    print("Result: ", x)
