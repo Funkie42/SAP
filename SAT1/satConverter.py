97edc84ad1ae419a28d832333a92180823449011
Funkie42 <marius.funk@gmx.de>
2018-11-10 13:54:14 +0100

    added writing of CNF-Array to .cnf file

--- SAT1/satConverter.py
+++ SAT1/satConverter.py
@@ -1,11 +1,35 @@
 import readFile
+import writeFile
 
 def convertToSat():
     # Read input data to Array "data"
     data = readFile.readFileData()
     print(data)
-    
 
+    '''
+    TODO LOGIC TO convert to CNF
+    
+    
+    '''
+    cnf = [[1,-3],[2,3,-1]] #Dummy version
+
+    #number of literals and terms
+
+    lits = 3
+    terms = len(cnf)
+
+    #write converted CNF to File
+    filenameCNF = writeFile.writeCNF(lits, terms, cnf)
+
+    '''TODO'''
+    #Open PicoSat and get solution for CNF in Array
+    solvedData = usePicoSat(filenameCNF)
+
+
+
+#Open PicoSat and get solution for CNF in Array
+def usePicoSat(filenameCNF):
+    return 0
 
 if __name__ == "__main__":
     convertToSat()
---
+++ SAT1/writeFile.py
@@ -0,0 +1,11 @@
+#write converted CNF to File
+def writeCNF(lits,terms, cnfArray):
+    f = open("satTest.cnf","w+")
+    f.write("p cnf " + str(lits) + " " + str(terms)+"\n")
+
+    for i in range(0,len(cnfArray)):
+        for j in range(0, len(cnfArray[i])):
+            f.write(str(cnfArray[i][j])+" ")
+        f.write("0\n")
+
+
